a30922c2d4742ff4762c7120989112ed3b727056
caiomaia83 <caiomaia2005.ms@gmail.com>
2025-11-25 14:55:53 -0300

    adicionando dependencias do room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,11 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Dependência do Room
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version") 
+    ksp("androidx.room:room-compiler:$room_version")
 }
